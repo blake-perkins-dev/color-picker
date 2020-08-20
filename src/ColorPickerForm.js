a2eaa80cb25794fba52cc421d5b9f9cda0f1a39d
blake-perkins-dev <blake_perkins@rocketmail.com>
2020-08-19 21:30:08 -0500

    style edits

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -3,26 +3,7 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/styles';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { ChromePicker } from 'react-color';
-
-const styles = {
-    container: {
-        width: "100%"
-    },
-    picker: {
-        width: "100% !important",
-        marginTop: "2rem"
-    },
-    addColor: {
-        width: "100%",
-        padding: "1rem",
-        marginTop: "1rem",
-        fontSize: "2rem"
-    },
-    colorNameInput: {
-        width: "100%",
-        height: "70px"
-    }
-}
+import styles from './styles/ColorPickerFormsStyles';
 
 class ColorPickerForm extends Component {
     constructor(props) {
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,50 +8,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import KeyboardTab from '@material-ui/icons/KeyboardTab';
+import Icon from '@material-ui/core/Icon';
 import Button from '@material-ui/core/Button';
-
-const drawerWidth = 400;
-
-const styles = theme => ({
-    root: {
-        display: "flex"
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        height: "64px"
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 20,
-    },
-    hide: {
-        display: "none"
-    },
-    navBtns: {
-        marginRight: "1rem",
-        "& a": {
-            textDecoration: "none"
-        }
-    },
-    btn: {
-        margin: "0 0.5rem"
-    }
-});
+import styles from './styles/PaletteFormNavStyles';
 
 class PaletteFormNav extends Component {
     constructor(props) {
@@ -89,9 +49,11 @@ class PaletteFormNav extends Component {
                             color="inherit"
                             aria-label="Open drawer"
                             onClick={handleDrawerOpen}
-                            className={classNames(classes.menuButton, open && classes.hide)}
+                            className={classNames(classes.menuButton, {
+                                [classes.hide]: open
+                            })}
                         >
-                            <MenuIcon />
+                            <KeyboardTab />
                         </IconButton>
                         <Typography variant="h6" color="inherit" noWrap>
                             Create A Palette
